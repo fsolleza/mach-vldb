6fc98833d3364c2d068bb8d96366c4835713e013
Franco Solleza <iam@francosolleza.com>
2024-03-17 17:04:47 +0000

    some threading stuff

--- rust/rocksdb/src/main.rs
+++ rust/rocksdb/src/main.rs
@@ -45,7 +45,7 @@ fn do_read(db: &DB, key: u64, expected: &[u8]) {
     compiler_fence(SeqCst);
     let res = db.get_pinned(key.to_be_bytes()).unwrap().unwrap();
     let sl: &[u8] = &*res;
-    assert_eq!(sl, *expected);
+    assert_eq!(sl, expected);
     compiler_fence(SeqCst);
 
     let dur = now.elapsed();
@@ -72,15 +72,15 @@ fn do_write(db: &DB, key: u64, slice: &[u8], opt: &WriteOptions) {
     println!("{:?}", log);
 }
 
-fn do_work(db: DB, read_ratio: f64, min_key: usize, max_key: usize) {
+fn do_work(db: DB, read_ratio: f64, min_key: u64, max_key: u64) {
     let data = random_data(1024 * 4);
-    let mut available_keys: HashMap<usize, &[u8]> = HashMap::new();
+    let mut available_keys: HashMap<u64, &[u8]> = HashMap::new();
     let mut rng = thread_rng();
     let mut opt = WriteOptions::default();
     opt.set_sync(true);
 
     loop {
-        let key = rng.gen_range(min_key..max_key);
+        let key: u64 = rng.gen_range(min_key..max_key);
         let read: bool = rng.gen::<f64>() < read_ratio;
 
         if read {
@@ -98,8 +98,25 @@ fn do_work(db: DB, read_ratio: f64, min_key: usize, max_key: usize) {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let threads = 8;
+    let keys_per_thread = 4096;
+    let read_ratio = 0.90;
+
     let path = PathBuf::from("/nvme/data/tmp/2024-vldb/rocksdb-app");
     let db = setup_db(path);
-    do_work(db, 0.90, 0, 4096);
+    let mut handles = Vec::new();
+
+    for i in 0..threads {
+        let s = i * keys_per_thread;
+        let e = s + keys_per_thread;
+        let db = db.clone();
+
+        handles.push(thread::spawn(move || {
+            do_work(db, read_ratio, s, e);
+        }));
+    }
+
+    for h in handles {
+        let _ = h.join();
+    }
 }
