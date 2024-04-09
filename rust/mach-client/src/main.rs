646437ae30e2c7cb03be5acfc3bc1c4474c2d02e
Franco Solleza <iam@francosolleza.com>
2024-04-09 12:26:43 +0000

    mach-test so that we can work on aggregation separately

---
+++ rust/mach-client/src/main.rs
@@ -0,0 +1,33 @@
+use std::thread;
+use std::time::Duration;
+use common::data::*;
+
+pub fn enable_kvlogs() {
+	println!("ENABLING KV LOGS IN COLLECTOR");
+	let mut connection =
+		common::ipc::ipc_client_connect("0.0.0.0:3011").unwrap();
+	let request = workload::AppRequest::Enable;
+	let _response: workload::AppResponse =
+		common::ipc::ipc_send(&request, &mut connection).unwrap();
+}
+
+fn run_request(min_ts: u64, max_ts: u64) {
+	let mut connection =
+		common::ipc::ipc_client_connect("0.0.0.0:3021").unwrap();
+	let request = mach_server::MachRequest::ByCpu(min_ts, max_ts);
+	let response: mach_server::MachResponse =
+		common::ipc::ipc_send(&request, &mut connection).unwrap();
+	let result = response.from_cpu().unwrap();
+	println!("{:?}", result);
+}
+
+fn main() {
+	enable_kvlogs();
+	thread::sleep(Duration::from_secs(2));
+	loop {
+		let now = micros_since_epoch();
+		let min_ts = now - 10 * 1_000_000;
+		run_request(min_ts, now);
+		thread::sleep(Duration::from_secs(1));
+	}
+}
